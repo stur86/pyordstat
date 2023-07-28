2ec71807b3724d767c71f0671a604f533be2a1ed
Simone Sturniolo <simonesturniolo@gmail.com>
2023-07-28 19:00:43 +0100

    Rebuilt docs, added imports

--- src/pyordstat/__init__.py
+++ src/pyordstat/__init__.py
@@ -1,5 +1,6 @@
 """pyordstat package."""
 from pyordstat.continuous import ContinuousOrderStatistics
+from pyordstat.discrete import DiscreteOrderStatistics
 from pyordstat.finite import FiniteOrderStatistics
 
-__all__ = ["ContinuousOrderStatistics", "FiniteOrderStatistics"]
+__all__ = ["ContinuousOrderStatistics", "FiniteOrderStatistics", "DiscreteOrderStatistics"]
